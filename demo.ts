3b339dd7c9d85b27643ff59a8709bcfa7f28d497
李灵菲 <lilingfei@bytedance.com>
2021-08-05 11:47:24 +0800

    mod.ts

--- demo.ts
+++
@@ -1,8 +0,0 @@
-addEventListener("fetch", (e: any) => {
-  const response = new Response("Hello world Lee", {
-    headers: {
-      "content-type": "text/plain",
-    },
-  });
-  e.respondWith(response);
-});
